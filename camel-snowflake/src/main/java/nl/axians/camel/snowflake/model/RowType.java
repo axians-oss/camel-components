dc75487a8e63df3bc26569eef59b399f98b8b6a5
Jacob Hoeflaken <jacob.hoeflaken@axians.com>
2024-06-24 22:23:49 +0200

    Added full Snowflake API support and POJO for results.

---
+++ camel-snowflake/src/main/java/nl/axians/camel/snowflake/model/RowType.java
@@ -0,0 +1,12 @@
+package nl.axians.camel.snowflake.model;
+
+public class RowType {
+
+    private String name;
+    private String type;
+    private long length;
+    private long precision;
+    private long scale;
+    private boolean nullable;
+
+}
